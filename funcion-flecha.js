693c30a7d1b96ea989284906cb44fa1d3b4a8414
yoyoH-111 <hernandezquiceno3@gmail.com>
2025-08-12 19:34:00 -0500

    estamos aprendiendo a crear una flecha

---
+++ funcion-flecha.js
@@ -0,0 +1,11 @@
+function nombredelafuncion(n1,n2){
+    console.log("los numero son:",n1,n2)
+}
+const variable =()=>{
+
+}
+const imprimirNombre =(nombre)=>{
+return "hola"+nombre;
+}
+resultado=imprimirNombre("Yoyo")
+console.log(resultado);
